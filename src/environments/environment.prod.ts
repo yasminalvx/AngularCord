22e0a839ea325c7fc4e7ae9cb7085e81032b2e73
Yasmin Alves <yasilvalves@gmail.com>
2022-03-02 23:10:39 -0300

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: "https://qgbrxsyjmhjdpnhpdtbq.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnYnJ4c3lqbWhqZHBuaHBkdGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDYxODg5MTksImV4cCI6MTk2MTc2NDkxOX0.AUizgLLgzGMerYjRTN6-WAXcTa2nRCO0gr_TBRhSPsw"
 };
